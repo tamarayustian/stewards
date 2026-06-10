552448d073286585b77ff0eaee208127a14178d7
Tamara Yustian <tamarayustian@gmail.com>
2026-06-11 02:52:56 +0800

    fix: change to session pooler connection

--- lib/db.ts
+++ lib/db.ts
@@ -1,3 +1,4 @@
+import { Pool } from "pg";
 import { PrismaClient } from "../lib/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
@@ -5,9 +6,12 @@ const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
 };
 
-const adapter = new PrismaPg({
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
+
+const adapter = new PrismaPg(pool);
 
 const prisma =
   globalForPrisma.prisma ||
